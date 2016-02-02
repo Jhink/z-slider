87f9db8d89a1a721116ff3e292f28c2e396585fc
James Bond <jhemerson.comercial@gmail.com>
2016-02-02 18:43:25 -0200

    Added functions for Navigation, Blocking Native Scroll and Applying Transform Animations

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -196,6 +196,85 @@
 			scrollContainer.scrollTop = yscroll;
 			scrollContainer.scrollLeft = xscroll;
 		}
+		// END noscroll()
+
+		// applies the necessary transform value to scale the item up
+		function applyTransforms(el, nobodyscale) {
+			// zoomer area and scale value
+			var zoomerArea = el.querySelector('.zoomer__area'),
+				zoomerAreaSize = {width: zoomerArea.offsetWidth, height: zoomerArea.offsetHeight},
+				zoomerOffset = zoomerArea.getBoundingClientRect(),
+				scaleVal = zoomerAreaSize.width/zoomerAreaSize.height < win.width/win.height ? win.width/zoomerAreaSize.width : win.height/zoomerAreaSize.height;
+
+				if( bodyScale && !nobodyscale ) {
+					scaleVal /= bodyScale;
+				}
+
+				// apply transform
+				el.style.WebkitTransform = 'translate3d(' + Number(win.width/2 - (zoomerOffset.left + zoomerAreaSize.width / 2)) + 'px, ' + Number(win.height/2 - (zoomerOffset.top + zoomerAreaSize.height/2)) + 'px, 0) scale3d(' + scaleVal + ',' + scaleVal + ',1)';
+				el.style.transform = 'translate3d(' + Number(win.width/2 - (zoomerOffset.left + zoomerAreaSize.width/2)) + 'px, ' + Number(win.height/2 - (zoomerOffset.top + zoomerAreaSize.height/2)) + 'px, 0) scale3d('  + scaleVal + ',' + scaleVal + ', 1)';
+		}
+		// END ApplyTransforms()
+
+		// navigate the slider
+		function navigate(dir) {
+			var itemCurrent = items[current],
+				currentEl = itemCurrent.querySelector('.slide__mover'),
+				currentTitleEl = itemCurrent.querySelector('.slide__title');
+			
+			// update new current value
+			if( dir === 'right' ) {
+				current = current < itemsTotal-1 ? current + 1 : 0;
+			} else {
+				current = current > 0 ? current - 1 : itemsTotal-1;
+			}
+
+			var itemNext = items[current],
+				nextEl = itemNext.querySelector('.slide__mover'),
+				nextTitleEl = itemNext.querySelector('.slide__title');
+
+			// animate the current element out
+			dynamics.animate(currentEl, { opacity: 0,translateX: dir === 'right' ? -1 * currentEl.offsetWidth/2 : currentEl.offsetWidth/2, rotateZ: dir === 'right' ? -10 : 10 }, {
+				type: dynamics.spring,
+				duration: 2000,
+				friction: 600,
+				complete: function() {
+					dynamics.css(itemCurrent, { opacity: 0, visibility: 'hidden' });
+				}
+			});
+
+			// animate the current title out
+			dynamics.animate(currentTitleEl, {translateX: dir === 'right' ? -250 : 250, opacity : 0}, {
+				type: dynamics.bezier,
+				points: [{"x": 0, "y": 0, "cp": [{"x": 0.2, "y": 1}]}, {"x": 1, "y": 1, "cp": [{"x":0.3, "y":1}]}],
+				duration: 450
+			});
+
+			// set the right properties for the next element to come in
+			dynamics.css(itemNext, { opacity: 1, visibility: 'visible' });
+			dynamics.css(nextEl, { opacity: 0, translateX: dir === 'right' ? nextEl.offsetWidth/2 : -1*nextEl.offsetWidth/2, rotateZ: dir === 'right' ? 10 : -10});
+
+			// animate the next element in
+			dynamics.animate(nextEl, { opacity: 1, translateX: 0 }, {
+				type: dynamics.spring,
+				duration: 2000,
+				friction: 600,
+				complete: function() {
+					items.forEach( function(item) { classie.remove(item, 'slide--current'); });
+					classie.add(itemNext, 'slide--current');
+				}
+			});
+
+			// set the right properties for the next title to come in
+			dynamics.css(nextTitleEl, { translateX: dir === 'right' ? 250 : -250, opacity: 0 });
+			// animate the next title in
+			dynamics.animate(nextTitleEl, { translateX: 0, opacity: 1}, {
+				type: dynamics.bezier, 
+				points: [{"x": 0, "y": 0, "cp": [{"x": 0.2, "y": 1}]}, {"x": 1, "y": 1, "cp": [{"x": 0.3, "y": 1}]}],
+				duration: 650
+			});
+		}
+		// END navigate()
 
 		init();
 }) (window); 
